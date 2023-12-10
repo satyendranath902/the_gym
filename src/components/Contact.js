4e988f7e7b052a57b044fb0444805b3575c064ae
satyendranath902 <satyendranath902@gmail.com>
2023-12-10 19:14:51 +0530

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,46 @@
 import React from 'react';
 
-const Contact = () => {
-  return (
-    <div>
-      <h2>100 ft road, Gunjur</h2>
-    </div>
-  );
-};
+class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 'Contact number',
+      count: 0
+    }
+
+    this.changeMessage = this.changeMessage.bind(this);
+    this.changeHideMessage = this.changeHideMessage.bind(this);
+  }
+
+  changeMessage() {
+    this.setState({
+      number: '8296454255',
+      count: this.state.count + 1,
+    }  
+    )
+    console.log('Before Callback value', this.state.count);
+  }
+
+  changeHideMessage() {
+    this.setState({
+        number: 'Contact number',
+    }  
+    )
+   
+  }
+
+  render() {
+    return (
+      <div >
+        <button onClick={this.changeMessage}>show number</button>
+        <button onClick={this.changeHideMessage}>hide number</button>
+        <h3>{this.state.number}</h3>
+        <h4>100 ft road, Gunjur</h4>
+        <h2>Number of Visitor:- {this.state.count}</h2>
+      </div>
+    );
+  }
+}
 
 export default Contact;
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -2,30 +2,9 @@ import React from 'react';
 import qr from '../qr.jpeg';
 
 class Payment extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 'GooglePay/PhonePay',
-      count: 0
-    }
-  }
-
-  changeMessage() {
-    this.setState({
-      number: '8296454255',
-      count: this.state.count + 1,
-    }  
-    )
-    console.log('Before Callback value', this.state.count);
-  }
-
   render() {
     return (
       <div >
-        <button onClick={() => this.changeMessage()}>Display number</button>
-        <h3>{this.state.number}</h3>
-        <h2>Number of Visitor:- {this.state.count}</h2>
         <img class='photo' src={qr} alt="app-logo" />
       </div>
     );
